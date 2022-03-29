0beee490aaaa0f76e00d53e667bd3b4c593ab3fa
Tiago Cabral <tiagorc.cabral@gmail.com>
2022-03-28 22:10:00 -0300

    :sparkles: adds solution to valid subsequence problem

--- arrays/validate_subsequence.go
+++ arrays/validate_subsequence.go
@@ -6,11 +6,18 @@ package arrays
 func IsValidSubsequence(array []int, sequence []int) bool {
 	sequenceIdx := 0
 	arrayIdx := 0
+	isSubsequence := false
 
 	for arrayIdx < len(array) && sequenceIdx < len(sequence) {
 		if array[arrayIdx] == sequence[sequenceIdx] {
 			sequenceIdx++
 		}
+		arrayIdx++
 	}
-	return false
+
+	if sequenceIdx == len(sequence) {
+		isSubsequence = true
+	}
+
+	return isSubsequence
 }
