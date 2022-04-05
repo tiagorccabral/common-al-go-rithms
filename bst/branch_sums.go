d5943149b46439bf2a840e376e72ba15e8f80444
Tiago Cabral <tiagorc.cabral@gmail.com>
2022-04-05 19:46:53 -0300

    :construction: wip: branch sums problem

---
+++ bst/branch_sums.go
@@ -0,0 +1,16 @@
+package bst
+
+// Category: bsts
+// Difficulty: Easy
+
+// This is the struct of the input root.
+type BinaryTree struct {
+	Value int
+	Left  *BinaryTree
+	Right *BinaryTree
+}
+
+func BranchSums(root *BinaryTree) []int {
+
+	return nil
+}
