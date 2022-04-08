9b0191f43041f868e776bdc7425d441b0fe60bd0
Tiago Cabral <tiagorc.cabral@gmail.com>
2022-04-07 21:51:13 -0300

    :memo: adds documentation to branch sums problem

--- bst/branch_sums.go
+++ bst/branch_sums.go
@@ -3,6 +3,8 @@ package bst
 // Category: bsts
 // Difficulty: Easy
 
+// Write a function that takes a Binary tree and returns a list of its branch sums ordered from leftmost to rightmost branch sum
+
 // This is the struct of the input root.
 type BinaryTree struct {
 	Value int
