530b144b3337d1130ee4f6e62cb4bfdc82095612
Tiago Cabral <tiagorc.cabral@gmail.com>
2022-05-09 19:20:59 -0300

    :sparkles: adds dfs problem

---
+++ graphs/depth_first_search.go
@@ -0,0 +1,17 @@
+package graphs
+
+type Node struct {
+	Name     string
+	Children []*Node
+}
+
+func (n *Node) DepthFirstSearch(array []string) []string {
+
+	array = append(array, n.Name)
+
+	for _, child := range n.Children {
+		array = child.DepthFirstSearch(array)
+	}
+
+	return array
+}
