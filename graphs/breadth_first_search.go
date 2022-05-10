c87ac30974b00956789c1c71a453e709ddfe6c90
Tiago Cabral <tiagorc.cabral@gmail.com>
2022-05-10 16:54:57 -0300

    :sparkles: adds bfs solution

---
+++ graphs/breadth_first_search.go
@@ -0,0 +1,21 @@
+package graphs
+
+type Node struct {
+	Name     string
+	Children []*Node
+}
+
+func (n *Node) BreadthFirstSearch(array []string) []string {
+	queue := []*Node{n}
+
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+		array = append(array, current.Name)
+
+		for _, children := range current.Children {
+			queue = append(queue, children)
+		}
+	}
+	return array
+}
