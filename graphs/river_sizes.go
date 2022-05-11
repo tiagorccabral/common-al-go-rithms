abd4416ccc85102adeeab3ea9c07e4db82777b25
Tiago Cabral <tiagorc.cabral@gmail.com>
2022-05-11 09:43:57 -0300

    :memo: adds river sizes problem especification

---
+++ graphs/river_sizes.go
@@ -0,0 +1,23 @@
+package graphs
+
+// Category: graphs
+// Difficulty: Medium
+
+// Given a matrix of 1s and 0s, where 1s adjecent to each other represent a continuous river (diagonal do not count as a continuous river). 
+// Should return an array with all the lengths of possible rivers.
+
+// Example: 
+// Input: matrix = [
+// 	[1, 0, 0, 1, 0],
+// 	[1, 0, 1, 0, 0],
+// 	[0, 0, 1, 0, 1],
+// 	[1, 0, 1, 0, 1],
+// 	[1, 0, 1, 1, 0]
+// ]
+
+// Output: [1, 2, 2, 2, 5]
+
+func RiverSizes(matrix [][]int) []int {
+	// Write your code here.
+	return nil
+}
